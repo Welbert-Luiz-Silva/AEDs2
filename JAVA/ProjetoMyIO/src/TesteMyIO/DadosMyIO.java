65661309f41fb74cf41267bf1b35512a41f73c2e
Welbert Luiz <113536868+Welbert-Luiz-Silva@users.noreply.github.com>
2023-03-08 19:39:46 -0300

    Atualização

---
+++ JAVA/ProjetoMyIO/src/TesteMyIO/DadosMyIO.java
@@ -0,0 +1,13 @@
+package TesteMyIO;
+
+public class DadosMyIO {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    int inteiro = MyIO.readInt("Entre com um inteiro: ");
+	    MyIO.println("Seu inteiro: " + inteiro);
+
+	}
+	
+
+}
