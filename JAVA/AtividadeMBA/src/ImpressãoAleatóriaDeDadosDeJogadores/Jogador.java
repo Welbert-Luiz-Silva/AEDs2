264d17b04ea38cad6585e2a34b88b4d71d5d0abd
Welbert Luiz <welbert.luiz.silva@gmail.com>
2023-03-23 17:33:37 -0300

    AtualizaçãoMBA

---
+++ JAVA/AtividadeMBA/src/ImpressãoAleatóriaDeDadosDeJogadores/Jogador.java
@@ -0,0 +1,13 @@
+package ImpressãoAleatóriaDeDadosDeJogadores;
+
+public class Jogador {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyIO.setCharset("UTF-8");
+		MyIO.println("Olá! Mundo.");
+		
+
+	}
+
+}
---
+++ t/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    int x;
+    x= 2+3;
+    return 0;
+}
