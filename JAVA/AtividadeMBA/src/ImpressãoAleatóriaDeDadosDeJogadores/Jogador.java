ca56f3ae7ba01248884a869fa0e67e80b10c0ed4
Welbert Luiz <welbert.luiz.silva@gmail.com>
2023-03-23 22:34:06 -0300

    Criando Classe Jogador

--- JAVA/AtividadeMBA/src/ImpressãoAleatóriaDeDadosDeJogadores/Jogador.java
+++ JAVA/AtividadeMBA/src/ImpressãoAleatóriaDeDadosDeJogadores/Jogador.java
@@ -1,13 +1,101 @@
 package ImpressãoAleatóriaDeDadosDeJogadores;
 
+import java.util.*;  
+
 public class Jogador {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		MyIO.setCharset("UTF-8");
-		MyIO.println("Olá! Mundo.");
+	private int id, altura, peso, anoNascimento;
+	private String nome, universidade, cidadeNascimento, estadoNascimento;
 		
-
+	
+public int getId() {
+		return id;
 	}
 
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public int getAltura() {
+		return altura;
+	}
+
+
+	public void setAltura(int altura) {
+		this.altura = altura;
+	}
+
+	public int getPeso() {
+		return peso;
+	}
+
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+
+	public int getAnoNascimento() {
+		return anoNascimento;
+	}
+
+	public void setAnoNascimento(int anoNascimento) {
+		this.anoNascimento = anoNascimento;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUniversidade() {
+		return universidade;
+	}
+
+	public void setUniversidade(String universidade) {
+		this.universidade = universidade;
+	}
+
+	public String getCidadeNascimento() {
+		return cidadeNascimento;
+	}
+
+	public void setCidadeNascimento(String cidadeNascimento) {
+		this.cidadeNascimento = cidadeNascimento;
+	}
+
+	public String getEstadoNascimento() {
+		return estadoNascimento;
+	}
+
+	public void setEstadoNascimento(String estadoNascimento) {
+		this.estadoNascimento = estadoNascimento;
+	}
+	
+	/*
+	 CRIAR METODO LER
+	public static criarJogador (Strig, ) {
+	
+	iNCLUIR MÉTODO TOKEM
+	}
+	System.*/
+
 }
+
+
+
+
+
+
+
+/*
+   public static void token (String[] args) {  
+       StringTokenizer st = new StringTokenizer("my,name,is,khan");  
+        
+      // printing next token  
+      System.out.println("Next token is : " + st.nextToken(","));  
+   }  
+ */
+
+ 
